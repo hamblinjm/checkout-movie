47fbb33a2af16b392af856bacb3122e20c6348df
Jessica Hamblin <hamblinjm@hotmail.com>
2021-04-02 15:05:16 -0600

    vue set up

---
+++ front-end/src/components/Movies.vue
@@ -0,0 +1,45 @@
+<template>
+    <div class="movies">
+        <!-- <div v-for="movie in movies" :key="movie.id">
+            <img :src="'images/' +movie.image">
+            <h3>{{movie.title}}</h3>
+        </div> -->
+        <router-link v-for="movie in movies" :key="movie.id" :to="'/movie/' + movie.id" style="display: inline-block;text-decoration:none;">
+            <img :src="'images/' +movie.image">
+            <h3>{{movie.title}}</h3>
+        </router-link>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'MovieList',
+    props: {
+        movies: Array
+    }
+}
+</script>
+
+<style scoped>
+/* .movie {
+    width: 300px;
+} */
+
+.movies {
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: space-around;
+}
+
+li a {
+    color: white;
+    text-decoration: none;
+}
+
+h3 {
+    color: white;
+    text-decoration: none;
+    width: 182px;
+}
+
+</style>
--- front-end/src/views/Home.vue
+++ front-end/src/views/Home.vue
@@ -1,18 +1,147 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <!-- <img class="home-img" src="/images/main-cropped.jpg"> -->
+    <h1>Movie Picker</h1>
+    <hr/>
+    <div class="filter-wrapper">
+      <h2>Filter by:</h2>
+      <div class="filter-grid">
+        <div class="filter-box">
+          <h3>MPA Rating</h3>
+          <input type="radio" id="g" value="G" v-model="mpa">
+          <label for="g">G</label>
+          <br>
+          <input type="radio" id="pg" value="PG" v-model="mpa">
+          <label for="pg">PG</label>
+          <br>
+          <input type="radio" id="pg13" value="PG-13" v-model="mpa">
+          <label for="pg13">PG-13</label>
+          <br>
+          <input type="radio" id="other" value="Other" v-model="mpa">
+          <label for="other">Other</label>
+          <br>
+          <span>Selected: {{ mpa }}</span>
+        </div>
+        <div class="filter-box">
+          <h3>Genre</h3>
+          <input type="radio" id="drama" value="drama" v-model="genre">
+          <label for="one">Drama</label>
+          <br>
+          <input type="radio" id="comedy" value="comedy" v-model="genre">
+          <label for="two">Comedy</label>
+          <br>
+          <input type="radio" id="horror" value="horror" v-model="genre">
+          <label for="two">Horror</label>
+          <br>
+          <input type="radio" id="thriller" value="thriller" v-model="genre">
+          <label for="two">Thriller</label>
+          <br>
+          <input type="radio" id="scifi" value="scifi" v-model="genre">
+          <label for="two">Scifi</label>
+          <br>
+          <input type="radio" id="fantasy" value="fantasy" v-model="genre">
+          <label for="two">Fantasy</label>
+          <br>
+          <span>Selected: {{ genre }}</span>
+        </div>
+
+      </div>
+      <button id="clear" v-on:click="clear">Clear Filters</button>
+
+    </div>
+    <MovieList :movies="movies" />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+// import HelloWorld from '@/components/HelloWorld.vue'
+import MovieList from '@/components/Movies.vue'
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld
+    MovieList
+  },
+  data() {
+    return {
+      mpa: '',
+      genre: ''
+    }
+  },
+  computed: {
+    movies() {
+      let filteredMovies = this.$root.$data.movies;
+      // if (this.mpa === '' && this.genre === '') {
+      //   return this.$root.$data.movies;
+      // } else if (this.mpa === '') {
+      //   return this.$root.$data.movies.filter(movie => movie.genre === this.genre);
+      // }
+
+      if (this.mpa !== '') {
+        filteredMovies = filteredMovies.filter(movie => movie.mpa_rating === this.mpa);
+      }
+      if (this.genre !== '') {
+        filteredMovies = filteredMovies.filter(movie => movie.genre === this.genre);
+      }
+      return filteredMovies;
+    }
+  },
+  methods: {
+    select(mpa, genre) {
+      this.mpa = mpa;
+      this.genre = genre;
+    },
+    clear() {
+      this.mpa = '';
+      this.genre = '';
+    }
   }
 }
 </script>
+
+<style scoped>
+.home-img {
+  width: 100%;
+  height: 400px;
+}
+
+.filter-grid {
+  display: flex;
+  justify-content: center;
+  flex-wrap: wrap;
+  flex-direction: row;
+  align-items: top;
+  margin-bottom: 30px;
+}
+
+.filter-box {
+  width: 200px;
+}
+
+h3 {
+  margin-top: 0;
+}
+
+h1 {
+  margin: 50px;
+  color: rgb(130, 201, 168);
+}
+
+hr {
+  background-color: rgb(130, 201, 168);
+}
+
+#clear {
+  margin-bottom: 30px;
+}
+
+hr {
+  /* width: 50%; */
+}
+
+button {
+  font-family: 'Staatliches', cursive;
+  width: 100px;
+}
+</style>
